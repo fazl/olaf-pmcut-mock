cd6e2ad532f3aa6bd72667db11186cdd8577638f
Fazl <5173704+fazl@users.noreply.github.com>
2021-01-16 19:17:25 +0000

    Minor script oops

--- bin/link.sh
+++ bin/link.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mkdir build 2>/dev/null
-cd build
+
+
 # Bar.o omitted as Bar is mocked in UnitTest.cpp
-g++ UnitTest.o PMCUTMock.o TypeWrapper.o Foo.o -o UnitTest.exe
+cd build && g++ UnitTest.o PMCUTMock.o TypeWrapper.o Foo.o -o UnitTest.exe
 
