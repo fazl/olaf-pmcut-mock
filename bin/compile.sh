df14171e80b6d1f198072d092205307a8a76fb5f
Fazl <5173704+fazl@users.noreply.github.com>
2021-01-18 11:15:51 +0000

    Compile with -D_GLIBCXX_DEBUG to help check STL calls

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+# TODO add forcing debug mode (maybe map key missing will tell which key missing? Hunt stackover how to..)
+
 mkdir build 2>/dev/null
 cd build
 pwd
 c99 -c ../*.c
-g++ -c ../*.cpp
+# Allegedly -D_GLIBCXX_DEBUG helps to checks STL container operations 
+g++ -c -D_GLIBCXX_DEBUG ../*.cpp  
+
 
